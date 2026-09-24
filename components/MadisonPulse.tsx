7586ceae2a9e4464901981c7bda120da5a32225f
jondenham85-ui <jondenham85@gmail.com>
2026-09-24 18:41:40 -0500

    Create MadisonPulse.tsx

---
+++ components/MadisonPulse.tsx
@@ -0,0 +1,7 @@
+export default function MadisonPulse() {
+  return (
+    <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+      <div className="w-[300px] h-[600px] bg-neon/20 blur-xl animate-pulseVertical rounded-full" />
+    </div>
+  );
+}
