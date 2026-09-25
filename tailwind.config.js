bd609c8234434f6e8be4b6cb00d8078cd434156a
jondenham85-ui <jondenham85@gmail.com>
2026-09-25 10:27:47 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,16 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "mad-black": "#02040a",
+        "mad-teal": "#35f5ff",
+        "mad-muted": "#7b8b9b",
+      },
+    },
   },
   plugins: [],
 };
